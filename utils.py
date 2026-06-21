ec916dca4f636a9a22b54be75f7578d42481ae39
Danya332345 <danya.ishmuratov@gmail.com>
2026-06-21 18:40:17 +0500

    Delete utils.py

--- utils.py
+++
@@ -1,5 +0,0 @@
-import random
-
-def random_energy(min_val: float = 0.0, max_val: float = 100.0) -> float:
-    """Возвращает случайное значение энергии."""
-    return round(random.uniform(min_val, max_val), 1)
